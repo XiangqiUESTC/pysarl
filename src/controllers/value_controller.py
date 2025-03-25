0afbec0b307b54044c2dca1e7a5f6d016bd88e36
XiangqiUESTC <402214886@qq.com>
2025-03-25 09:41:46 +0800

    采用scheme类构建value_controller，修正了_build_inputs中对于离散情况的维度处理的不统一

--- src/controllers/value_controller.py
+++ src/controllers/value_controller.py
@@ -11,29 +11,38 @@ class ValueController(AbstractController):
     def __init__(self, args, scheme):
         super().__init__(args, scheme)
 
+        # 保存args参数和scheme
         self.args = args
+        self.scheme = scheme
 
-        self.state_shape = scheme['state_info']["shape"]
-        self.state_dim = scheme['state_info']["dim"]
-        self.action_shape = scheme['action_info']["shape"]
+        # 从scheme中获得一些必要的参数
+        if scheme.state_space_type == 'continuous':
+            self.state_dim = len(scheme.continuous_state_shape)
+        elif scheme.state_space_type == 'discrete':
+            self.state_dim = 1
+        else:
+            raise NotImplementedError("State space type not supported!")
 
+        # 构建agent
         self._build_agent()
+        # 初始化动作选择器
         self.action_selector = ACTION_SELECTOR_REGISTRY[args.action_selector](args)
 
     def select_action(self, batch, t_env, t, test_mode=False):
         flat_states = self._build_inputs(batch)
         agents_outputs = self.forward(flat_states)
+        print(f"agent_outputs是{agents_outputs}")
         return self.action_selector.select_action(agents_outputs, t_env, t, test_mode=test_mode)
 
     def forward(self, flat_states):
         return self.agent(flat_states)
 
     def _build_agent(self):
-        state_size = np.prod(self.state_shape)
-        action_size = np.prod(self.action_shape)
-        self.agent = CRITIC_REGISTRY[self.args.critic](state_size, action_size)
+        self.agent = CRITIC_REGISTRY[self.args.critic](self.args, self.scheme)
 
     def _build_inputs(self, batch):
-        batch = torch.tensor(batch, dtype=torch.float32)
+        batch = torch.tensor(batch, dtype=torch.float)
         flat_states = torch.flatten(batch, start_dim=-self.state_dim)
+        if self.scheme.state_space_type == 'discrete':
+            flat_states.unsqueeze_(1)
         return flat_states

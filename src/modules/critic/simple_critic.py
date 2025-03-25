50072305ece1b4ec07148d4d82f7bd1acc791b48
XiangqiUESTC <402214886@qq.com>
2025-03-25 17:59:23 +0800

    所有的模块都应该由args和scheme决定如何构建

--- src/modules/critic/simple_critic.py
+++ src/modules/critic/simple_critic.py
@@ -1,14 +1,22 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 
 # simple_full_connect_actor
 class SimpleCritic(nn.Module):
-    def __init__(self, state_size, action_size, hidden_size=256):
+    def __init__(self, args, scheme):
+        action_size = scheme.discrete_action_size
+        if scheme.state_space_type == 'continuous':
+            state_size = np.prod(scheme.continuous_state_shape)
+        elif scheme.state_space_type == 'discrete':
+            state_size = 1
+        else:
+            raise NotImplementedError('Only discrete and discrete state space are supported!')
         super(SimpleCritic, self).__init__()
-        self.fc1 = nn.Linear(state_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, action_size)
+        self.fc1 = nn.Linear(state_size, args.hidden_size)
+        self.fc2 = nn.Linear(args.hidden_size, args.hidden_size)
+        self.fc3 = nn.Linear(args.hidden_size, action_size)
 
     def forward(self, states):
         x = F.relu(self.fc1(states))

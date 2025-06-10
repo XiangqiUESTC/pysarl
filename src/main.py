54eb3a986207b4df7983e9b68b5949b2f7ad9d43
XiangqiUESTC <402214886@qq.com>
2025-06-10 20:17:52 +0800

    main.py的重新梳理，包括移除了所有定义在main函数里面的函数方法，这些方法被移动到了utils.functions下。增加了很多必要的注释

--- src/main.py
+++ src/main.py
@@ -5,92 +5,72 @@ from sacred.observers import FileStorageObserver
 # 系统模块及工具模块
 import sys
 import os
-from os.path import dirname, abspath
+from os.path import dirname, abspath, join
 from copy import deepcopy
 import yaml
-from collections.abc import Mapping
 
 # 框架其他模块
 from utils.logging import get_logger
+from utils.functions import recursive_dict_update
+from utils.functions import get_config
 from run import run
+
 
 # 创建实验
 ex = Experiment("pysarl")
 
-# 储存实验结果的文件夹
-results_path = os.path.join(dirname(dirname(abspath(__file__))), "results")
-
-# 日志记录设置
-logger = get_logger()
-ex.logger = logger
-
-
-# 一些个工具函数
-
-# 这个方法用于读取命令参数中含有
-def _get_config(_params, arg_name, subfolder):
-    config_name = None
-    for _i, _v in enumerate(_params):
-        if _v.split("=")[0] == arg_name:
-            config_name = _v.split("=")[1]
-            del _params[_i]
-            break
-
-    if config_name is not None:
-        with open(os.path.join(os.path.dirname(__file__), "config", subfolder, "{}.yaml".format(config_name)), "r") as f:
-            try:
-                _config_dict = yaml.safe_load(f)
-            except yaml.YAMLError as _exc:
-                assert False, "{}.yaml error: {}".format(config_name, _exc)
-        return _config_dict
-
-
-# 这个方法用于递归地更新一个字典
-def recursive_dict_update(d, u):
-    if u is not None:
-        for k, v in u.items():
-            if isinstance(v, Mapping):
-                d[k] = recursive_dict_update(d.get(k, {}), v)
-            else:
-                d[k] = v
-    return d
-
-
-def config_copy(config):
-    if isinstance(config, dict):
-        return {k: config_copy(v) for k, v in config.items()}
-    elif isinstance(config, list):
-        return [config_copy(v) for v in config]
-    else:
-        return deepcopy(config)
-
 @ex.main
 def my_main(_run, _config, _log):
-    # 开始运行
+    """
+        Description: sacred实验函数，main.py主要处理参数和配置，处理完成之后主要的运行在run.py的run函数中
+        Arguments:
+            _run: 当前实验的运行对象
+            _config: 所有配置的字典
+            _log: Sacred创建的日志对象
+    """
+    # 调用run.py中的run函数开始运行
     run(_run, _config, _log)
 
 
 if __name__ == '__main__':
-    params = deepcopy(sys.argv)
-    # 根据main.py的路径（不管是相对还是绝对），找到默认的配置文件
-    with open(os.path.join(dirname(__file__), "config", "default.yaml")) as f:
+    # 日志记录设置
+    logger = get_logger()
+    ex.logger = logger
+
+    # 根据main.py的路径（不管是相对还是绝对），获取代码根目录src绝对路径和项目根目录绝对路径
+    abs_src_folder = abspath(dirname(__file__))
+    abs_proj_folder = dirname(dirname(abspath(__file__)))
+
+    # 读取默认配置
+    with open(os.path.join(abs_src_folder, "config", "default.yaml")) as f:
         try:
             config_dict = yaml.safe_load(f)
         except yaml.YAMLError as exc:
-            assert False, "dqn.yaml error: {}".format(exc)
+            assert False, "default.yaml error: {}".format(exc)
 
-    env_config = _get_config(params, "--env-config", "envs")
-    alg_config = _get_config(params, "--alg-config", "algs")
+    # 拷贝一份命令行参数，避免后续调用get_config函数时修改了sys.argv
+    params = deepcopy(sys.argv)
+    # 读取环境配置
+    env_config = get_config(params, "--env-config", join(abs_src_folder, "config/envs"))
+    # 读取算法配置
+    alg_config = get_config(params, "--alg-config", join(abs_src_folder, "config/algs"))
 
+    # 用环境配置递归地更新默认配置
     config_dict = recursive_dict_update(config_dict, env_config)
+    # 再用算法配置递归地更新默认配置
     config_dict = recursive_dict_update(config_dict, alg_config)
 
-    # now add all the config to sacred
+    # 把配置项加入实验配置中
     ex.add_config(config_dict)
 
     # Save to disk by default for sacred
     logger.info("实验结果将被保存在中results/sacred.")
-    file_obs_path = os.path.join(results_path, "sacred")
+
+    # 储存实验结果的文件夹
+    results_path = os.path.join(abs_proj_folder, "results")
+    file_obs_path = os.path.join(abs_proj_folder, "results", "sacred")
+
     ex.observers.append(FileStorageObserver.create(file_obs_path))
 
+    # 开始实验
     ex.run_commandline(params)

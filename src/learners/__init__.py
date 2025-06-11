70f624f695cd54c07a6bc32ab98554a09727f990
XiangqiUESTC <402214886@qq.com>
2025-06-11 16:18:04 +0800

    dqn方法的添加和完善，包括学习器，配置和注册

---
+++ src/learners/__init__.py
@@ -0,0 +1,4 @@
+from .dqn import DQN
+REGISTER = {
+    "dqn": DQN,
+}
---
+++ src/learners/dqn.py
@@ -0,0 +1,16 @@
+class DQN:
+    def __init__(self, args, scheme, controller, logger):
+        pass
+
+    def learn(self, buffer):
+        pass
+
+    def cuda(self):
+        pass
+
+    def save_models(self, path):
+        pass
+
+    def load_models(self, path):
+        pass
+

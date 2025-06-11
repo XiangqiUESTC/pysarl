7058d821bf3b32c6c3e17351db907c18aff323b8
XiangqiUESTC <402214886@qq.com>
2025-06-11 16:51:00 +0800

    DQN中关于buffer的一些代码段

--- src/buffers/basic_buffer.py
+++ src/buffers/basic_buffer.py
@@ -1,28 +1,37 @@
+from collections import deque
 
 
 #
 class BasicBuffer:
     def __init__(self, args, scheme):
-        self.data = {}
         self.args = args
         self.scheme = scheme
-        self.max_episode_length = 0
+
+        self.data = None
+        self.max_episode_length = None
+        self.episode_num = None
+
+        self.reset()
 
     def reset(self):
-        pass
+        self.data = {}
+        self.max_episode_length = 0
+        self.episode_num = 0
 
     def insert(self, transaction):
         if not isinstance(transaction, dict):
             raise TypeError("状态转移字典应为字典类型数据变量")
         else:
             for key, value in transaction.items():
-
                 if key not in self.data:
-                    self.data[key] = []
+                    self.data[key] = deque(maxlen=self.args.buffer_size)
                 self.data[key].append(value)
 
+        if self.episode_num < self.args.buffer_size:
+            self.episode_num += 1
+
     def can_sample(self):
-        pass
+        return self.episode_num >= self.args.buffer_size
 
     def sample(self):
         pass

3b8c2c8cd146f206a2b2f29285a44ccb9cc0fe91
XiangqiUESTC <402214886@qq.com>
2025-06-12 17:48:28 +0800

    基本实现了经验抽样的功能

--- src/buffers/basic_buffer.py
+++ src/buffers/basic_buffer.py
@@ -1,4 +1,8 @@
+import random
 from collections import deque
+from copy import deepcopy
+
+import torch
 
 
 #
@@ -34,4 +38,40 @@ class BasicBuffer:
         return self.episode_num >= self.args.buffer_size
 
     def sample(self):
-        pass
+        indices = random.sample(range(self.episode_num), self.args.batch_size)
+
+        sampled_data = {}
+
+        for key, dq in self.data.items():
+            # 高效地从deque中提取指定索引的元素
+            sampled_values = [torch.stack(dq[i]) for i in indices]
+            # 创建新的固定大小的deque
+            sampled_data[key] = sampled_values
+
+        # 根据最长的步长补全数据
+        max_episode_length = max([sum(self.data["filled"][i]) for i in indices]).item()
+
+        padded_batch = {}
+
+        # pad数据
+        for key, tensor_batch in sampled_data.items():
+            padded_batch[key] = []
+            for tensor_to_pad in tensor_batch:
+                print(f"tensor_to_pad_shape: {tensor_to_pad.shape}")
+                # 获得形状
+                pad_shape = list(tensor_to_pad.shape)
+                # 在第一个维度进行填充
+                pad_shape[0] = max_episode_length + 1 - pad_shape[0]
+                print(f"pad_shape: {pad_shape}")
+                # 创建用于填充的tensor
+                pad_tensor = torch.zeros(pad_shape, dtype=tensor_to_pad.dtype)
+                # 使用cat函数完成填充
+                padded = torch.cat([tensor_to_pad, pad_tensor], dim=0)
+                # 把填充后的tensor放回
+                padded_batch[key].append(padded)
+            padded_batch[key] = torch.stack(padded_batch[key])
+
+        return  padded_batch
+
+    def __getitem__(self, item):
+        return self.data[item]

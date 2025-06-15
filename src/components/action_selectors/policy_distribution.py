06c3c372c71b030308c55d3b1d811b297a6dfa25
XiangqiUESTC <402214886@qq.com>
2025-06-16 01:32:00 +0800

    策略采样动作选择器

--- src/components/action_selectors/policy_distribution.py
+++ src/components/action_selectors/policy_distribution.py
@@ -1,3 +1,5 @@
+import torch
+
 class PolicyDistribution:
     def __init__(self, args):
         self.args = args
@@ -5,4 +7,11 @@ class PolicyDistribution:
 
     def select_action(self, agent_output, t_env, t, test_mode=False):
         self.agent_output = agent_output
-        return agent_output.max(1)[1]
+
+        if test_mode:
+            actions = agent_output.max(-1)[1]
+        else:
+            action_dist = torch.distributions.Categorical(agent_output)
+            actions = action_dist.sample()
+
+        return actions

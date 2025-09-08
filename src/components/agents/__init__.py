687dad1a84b9a01eea491372f7e231d7ca4115f9
XiangqiUESTC <402214886@qq.com>
2025-09-08 13:43:22 +0800

    CNN ACTOR的添加

--- src/components/agents/__init__.py
+++ src/components/agents/__init__.py
@@ -1,9 +1,11 @@
 from .simple_critic import SimpleCritic
 from .q_table import QTable
 from .simple_actor import SimpleActor
+from .simple_cnn_actor import SimpleCNNActor
 
 REGISTRY = {
     "simple_critic": SimpleCritic,
     "simple_actor": SimpleActor,
     "q_table": QTable,
+    "simple_cnn_actor": SimpleCNNActor,
 }

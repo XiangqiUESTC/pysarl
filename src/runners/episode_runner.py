f546d790716143fb0916e0612009d9fa6245b3e3
XiangqiUESTC <402214886@qq.com>
2025-09-12 17:12:38 +0800

    truncate是一个用于游戏超时时进行阶段的标志，所以在交互的时候应该考虑这点

--- src/runners/episode_runner.py
+++ src/runners/episode_runner.py
@@ -12,7 +12,7 @@ class EpisodeRunner(AbstractRunner):
     # 这个函数会跑完一个完整的episode，收集整个episode的交互数据
     def run(self, test_mode=False):
         self.reset()
-        terminated = self.env.terminated
+        terminated = truncated = self.env.terminated
 
         total_reward = 0
 
@@ -27,7 +27,7 @@ class EpisodeRunner(AbstractRunner):
         }
         # 选择动作？
         state = self.env.get_state()
-        while not terminated:
+        while not (terminated or truncated):
             # 即使是一个state也要做batch化的处理，这是统一的要求
             batch_state = torch.unsqueeze(state, 0).to(self.args.device)
             actions = self.controller.select_action(batch_state, self.t_env, self.t, test_mode=test_mode)
@@ -40,7 +40,7 @@ class EpisodeRunner(AbstractRunner):
             transaction["filled"].append(torch.tensor([1]))
 
             # 执行动作，actions是针对一个batch的state返回的所有行动的集合，所以这里要取actions[0]
-            state, reward, terminated, info = self.env.step(actions.item())
+            state, reward, terminated, truncated, *_ = self.env.step(actions.item())
 
             # 累加奖励
             total_reward += reward
@@ -54,6 +54,8 @@ class EpisodeRunner(AbstractRunner):
 
         self.episode += 1
         self.t_env += self.t
+
+        self.logger.log_scalar("total_rewards", total_reward, self.t_env)
 
         if self.episode % self.args.log_interval == 0:
             self.logger.logger.info(f"第{self.episode}个episode的奖励为{total_reward}", )

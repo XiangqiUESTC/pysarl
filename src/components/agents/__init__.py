c7921bb11a157f0e1323624efcbf3949cf4718ec
XiangqiUESTC <402214886@qq.com>
2025-09-22 21:30:34 +0800

    增加了一个value critic

--- src/components/agents/__init__.py
+++ src/components/agents/__init__.py
@@ -2,10 +2,12 @@ from .simple_critic import SimpleCritic
 from .q_table import QTable
 from .simple_actor import SimpleActor
 from .simple_cnn_actor import SimpleCNNActor
+from .full_connect_value import FullConnectedValue
 
 REGISTRY = {
     "simple_critic": SimpleCritic,
     "simple_actor": SimpleActor,
     "q_table": QTable,
     "simple_cnn_actor": SimpleCNNActor,
+    "full_connect_value": FullConnectedValue
 }

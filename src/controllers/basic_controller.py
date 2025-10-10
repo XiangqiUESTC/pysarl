2cb968e15c41dd1b8033ed3c2b3de937d804c416
XiangqiUESTC <402214886@qq.com>
2025-10-10 13:45:35 +0800

    在basic controller中，删去了仅有一句话的_build_agent方法，直接将该行代码放入初始化方法中，同时，在初始化方法中，加入了encoder的初始化逻辑，便于处理不同环境的解码问题. 同时修改了select_action的部分逻辑，不再使用变量flat_states，在_build_inputs方法中，完成了最基本的选择当前state返回的逻辑

--- src/controllers/basic_controller.py
+++ src/controllers/basic_controller.py
@@ -3,6 +3,7 @@ import torch
 from .abstract_controller import AbstractController
 from components.agents import REGISTRY as agent_REGISTRY
 from components.action_selectors import REGISTRY as action_selector_REGISTRY
+from components.state_encoder import REGISTRY as state_encoder_REGISTRY
 
 
 # the controllers for vanilla value-based and policy-base rl algorithm
@@ -22,14 +23,22 @@ class BasicController(AbstractController):
         else:
             raise NotImplementedError("State space type not supported!")
 
-        # 构建agent
-        self._build_agent()
-        # 初始化动作选择器
+        ## 构建agent
+        self.agent = agent_REGISTRY[self.args.agent](self.args, self.scheme)
+
+        ## 初始化动作选择器
         self.action_selector = action_selector_REGISTRY[args.action_selector](args)
 
-    def select_action(self, batch, t_env, t, test_mode=False):
-        flat_states = self._build_inputs(batch)
-        agents_outputs = self.forward(flat_states)
+        ## 构建state_encoder
+        encoder_reg_name = getattr(args, "encoder", args.env_args.game_name)
+        if encoder_reg_name in state_encoder_REGISTRY:
+            self.state_encoder = state_encoder_REGISTRY[encoder_reg_name](self.args, self.scheme)
+        else:
+            self.state_encoder = None
+
+    def select_action(self, transaction_batch, t_env, t, test_mode=False):
+        agent_inputs = self._build_inputs(transaction_batch, t)
+        agents_outputs = self.forward(agent_inputs)
         return self.action_selector.select_action(agents_outputs, t_env, t, test_mode=test_mode)
 
     def forward(self, states):
@@ -38,11 +47,11 @@ class BasicController(AbstractController):
     def parameters(self):
         return self.agent.parameters()
 
-    def _build_agent(self):
-        self.agent = agent_REGISTRY[self.args.agent](self.args, self.scheme)
+    def _build_inputs(self, transaction_batch, t):
+        states = transaction_batch["states"]
+        some_state = states[t:t + 1]
 
-    def _build_inputs(self, batch):
-        return batch
+        return torch.stack(some_state)
 
     def cuda(self):
         self.agent.cuda()

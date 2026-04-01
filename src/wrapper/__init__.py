d590b150cb0de2fd2814d801d3c383061b01ed63
XiangqiUESTC <402214886@qq.com>
2026-04-01 19:41:28 +0800

    增加了一些必要的wrapper

--- src/wrapper/__init__.py
+++ src/wrapper/__init__.py
@@ -1,9 +1,6 @@
-import gymnasium as gym
-from gymnasium.wrappers.common import TimeLimit
 from .common import StateToTensor, RewardToTensor, AllToTensor, FlagToTensor
 
 REGISTRY = {
-    "time_limit": TimeLimit,
     "state_to_tensor": StateToTensor,
     "reward_to_tensor": RewardToTensor,
     "flag_to_tensor": FlagToTensor,

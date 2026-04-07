539a3fe11d9f6416f2c545ed1242437fa4325b8e
XiangqiUESTC <402214886@qq.com>
2026-04-07 21:56:21 +0800

    EpisodeRunner逻辑修改

--- src/runners/episode_runner.py
+++ src/runners/episode_runner.py
@@ -1,52 +1,70 @@
 import torch
 
-from .abstract_runner import AbstractRunner
+from buffers import REGISTRY as buffer_REGISTRY
+from controllers import REGISTRY as controller_REGISTRY
+from envs import REGISTRY as env_REGISTRY
 
 
-class EpisodeRunner(AbstractRunner):
+class EpisodeRunner:
+    """
+    一次只收集一个完整回合。
+    """
 
     def __init__(self, args, logger):
-        super().__init__(args, logger)
-        # 每次运行只得到一个episode
+        self.env = None
+        self.controller = None
+        self.buffer = None
+        self.env_scheme = None
+        self.buffer_scheme = None
+
+        self.args = args
+        self.logger = logger
+
+        self.t_env = 0
+        self.t = 0
+        self.episode = 0
         self.batch_size = 1
 
-    # 这个函数会跑完一个完整的episode，收集整个episode的交互数据
+        self.setup()
+
     def run(self, test_mode=False):
-        # 重置环境
         self.reset()
-        # 新增一个episode
         self.buffer.new_episode()
-        # 初始化终止符号和截断符号
-        terminated = truncated = self.env.terminated
-        # 记录reward
+
+        terminated = self.env.terminated
+        truncated = self.env.terminated
         total_reward = 0
-        # 选择动作？
         state = self.env.get_state()
+
         while not (terminated or truncated):
             self.buffer.insert(state, "state", self.t)
 
-            # 把列表类型的transaction数据包装成tensor返回，用于构建输入agent中的数据
-            actions = self.controller.select_action(self.buffer, self.t_env, self.t, test_mode=test_mode)
+            current_input = self.controller.builder.build_timestep(self.buffer, self.t)
+            self.buffer.insert(current_input, "input", self.t)
 
-            # 记录状态转移数据
-            self.buffer.insert(actions[0], "action", self.t)
+            action = self.controller.select_action(self.buffer, self.t_env, self.t, test_mode=test_mode)
+
+            self.buffer.insert(action[0], "action", self.t)
             self.buffer.insert(terminated, "terminated", self.t)
-
-            # 记录步长
+            self.buffer.insert(truncated, "truncated", self.t)
             self.buffer.insert(torch.tensor([1.0]), "filled", self.t)
 
-            # 执行动作，actions是针对一个batch的state返回的所有行动的集合，所以这里要取actions[0]
-            state, reward, terminated, truncated, *_ = self.env.step(actions.item())
-
-            # 累加奖励
+            state, reward, terminated, truncated, *_ = self.env.step(action.item())
             total_reward += reward
-
-            self.buffer.insert(reward, "rewards", self.t)
+            self.buffer.insert(reward, "reward", self.t)
 
             self.t += 1
-        # 记录终止信息
-        self.buffer.insert(state, "states", self.t + 1)
-        self.buffer.insert(terminated, "terminated", self.t + 1)
+
+        self.buffer.insert(state, "state", self.t)
+        self.buffer.insert(terminated, "terminated", self.t)
+        self.buffer.insert(truncated, "truncated", self.t)
+
+        final_input = self.controller.builder.build_timestep(self.buffer, self.t)
+        self.buffer.insert(final_input, "input", self.t)
+
+        if test_mode:
+            self.buffer.discard_last_episode()
+            return total_reward
 
         self.episode += 1
         self.t_env += self.t
@@ -54,5 +72,19 @@ class EpisodeRunner(AbstractRunner):
         self.logger.log_scalar("total_rewards", total_reward, self.t_env)
 
         if self.episode % self.args.log_interval == 0:
-            self.logger.logger.info(f"Episode: {self.episode:>5} t_env: {self.t_env:>10} total_reward: {total_reward}", )
+            self.logger.logger.info(
+                f"Episode: {self.episode:>5} t_env: {self.t_env:>10} total_reward: {total_reward}",
+            )
 
+        return total_reward
+
+    def setup(self):
+        self.env = env_REGISTRY[self.args.env](self.args)
+        self.env_scheme = self.env.get_scheme()
+        self.controller = controller_REGISTRY[self.args.controller](self.args, self.env_scheme)
+        self.buffer_scheme = self.controller.get_buffer_scheme()
+        self.buffer = buffer_REGISTRY[self.args.buffer](self.args, self.buffer_scheme)
+
+    def reset(self):
+        self.env.reset()
+        self.t = 0

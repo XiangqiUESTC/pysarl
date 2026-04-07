a186547670682385b652b76f37484eccd882637f
XiangqiUESTC <402214886@qq.com>
2026-04-07 22:08:20 +0800

    reinforce算法逻辑变动

--- src/learners/reinforce.py
+++ src/learners/reinforce.py
@@ -1,7 +1,6 @@
-from copy import deepcopy
-
 import torch
 from torch import optim
+
 from components.agents import REGISTRY
 
 
@@ -9,48 +8,43 @@ class Reinforce:
     def __init__(self, args, runner, logger):
         self.args = args
         self.runner = runner
+        self.logger = logger
+
         controller = runner.controller
 
         if args.baseline:
-            self.critic = REGISTRY[args.critic.name](args.critic, None)
+            self.critic = REGISTRY[args.critic.name](args.critic, controller)
             self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=args.critic_lr)
 
         self.optimizer = optim.Adam(controller.parameters(), lr=args.lr)
 
-
     def learn(self, buffer, t_env, episode_num):
-        batch = buffer.sample()
-        # 获取基本的数据结构
-        # terminated和states的长度比actions和rewards和filled多一个
+        batch = buffer.sample(granularity="episode")
 
-        # 需要所有的states
-        states = batch["states"] # batch_size × (episode_length+1) × state_dim
+        action = batch["action"][:, :-1]
+        reward = batch["reward"][:, :-1]
+        valid = batch["valid"][:, :-1]
 
-        # 最后一个动作、reward和filled都是无效的，填充的数据而已
-        actions = batch["actions"][:, :-1] # batch_size × episode_length × 1
-        rewards = batch["rewards"][:, :-1] # batch_size × episode_length × 1
-        filled = batch["filled"][:, :-1] # batch_size × episode_length × 1
+        action_probs = self.runner.controller.forward(batch)[:, :-1]
+        chosen_action_probs = torch.gather(action_probs, -1, action)
+        log_chosen_action_probs = torch.log(chosen_action_probs.clamp_min(1e-8))
 
-        # 获取选择动作的概率
-        action_probs = self.runner.controller.forward(states[:,:-1])
-        chosen_action_probs = torch.gather(action_probs, -1, actions)
-
-        # 根据formula选择3种不同的公式
         if self.args.formula == 1:
-            episode_rewards = (rewards * filled).sum(dim=1).squeeze(-1) # batch_size
-            log_chosen_action_probs_sum = torch.log(chosen_action_probs).sum(dim=1).squeeze(-1) # batch_size
-            loss = -(episode_rewards * log_chosen_action_probs_sum).sum()/filled.sum()
+            episode_reward = (reward * valid).sum(dim=1).squeeze(-1)
+            log_action_prob_sum = (log_chosen_action_probs * valid).sum(dim=1).squeeze(-1)
+            loss = -(episode_reward * log_action_prob_sum).sum() / valid.sum().clamp_min(1.0)
         elif self.args.formula == 2:
-            log_chosen_action_probs_cumsum = torch.log(chosen_action_probs).cumsum(dim=1)
-            loss = -(rewards*log_chosen_action_probs_cumsum).sum()/filled.sum()
+            log_action_prob_cumsum = (log_chosen_action_probs * valid).cumsum(dim=1)
+            loss = -(reward * log_action_prob_cumsum * valid).sum() / valid.sum().clamp_min(1.0)
         elif self.args.formula == 3:
-            log_chosen_action_probs = torch.log(chosen_action_probs)
-            return_sample = (rewards * filled).flip(1).cumsum(dim=1).flip(1)
+            return_sample = (reward * valid).flip(1).cumsum(dim=1).flip(1)
+
             if self.args.baseline:
-                state_vals = self.critic(states[:,:-1]).detach().clone()
-                loss = -(log_chosen_action_probs * (return_sample-state_vals) * filled).sum()/filled.sum()
+                critic_batch = self.runner.controller.slice_batch(batch, end_t=-1)
+                state_values = self.critic(critic_batch).detach().clone()
+                loss = -(log_chosen_action_probs * (return_sample - state_values) * valid).sum() / valid.sum().clamp_min(1.0)
             else:
-                loss = -(log_chosen_action_probs * return_sample * filled).sum()/filled.sum()
+                loss = -(log_chosen_action_probs * return_sample * valid).sum() / valid.sum().clamp_min(1.0)
         else:
             raise NotImplementedError("Method REINFORCE only has 3 kinds of formulas!")
 
@@ -62,16 +56,18 @@ class Reinforce:
         # 如果有基线还要优化基线
         # 以return_sample作为估计
         if self.args.baseline:
-            return_sample = (rewards * filled).flip(1).cumsum(dim=1).flip(1)
-            v_values = self.critic(states[:,:-1])
-            value_loss = ((return_sample - v_values)**2*filled).sum()/filled.sum()
+            return_sample = (reward * valid).flip(1).cumsum(dim=1).flip(1)
+            critic_batch = self.runner.controller.slice_batch(batch, end_t=-1)
+            value_prediction = self.critic(critic_batch)
+            value_loss = ((return_sample - value_prediction) ** 2 * valid).sum() / valid.sum().clamp_min(1.0)
+
             self.critic_optimizer.zero_grad()
             value_loss.backward()
             self.critic_optimizer.step()
 
-
     def cuda(self):
         self.runner.controller.cuda()
+
         if self.args.baseline:
             self.critic.cuda()
 
@@ -80,4 +76,3 @@ class Reinforce:
 
     def load_models(self, path):
         pass
-

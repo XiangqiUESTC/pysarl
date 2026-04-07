fb586f766fc9cd6fbaf66ca391b8fa4193f695f0
XiangqiUESTC <402214886@qq.com>
2026-04-07 22:05:32 +0800

    ac算法基本逻辑

--- src/learners/actor_critic.py
+++ src/learners/actor_critic.py
@@ -1,24 +1,19 @@
-from copy import deepcopy
-import torch
 from torch import optim
 
 from components.agents import REGISTRY
+
 
 class ActorCritic:
     def __init__(self, args, runner, logger):
         # 保存参数
         self.args = args
         self.runner = runner
+        self.logger = logger
 
+        self.critic = REGISTRY[args.critic](args, self.runner.controller)
 
-
-        # 定义critic
-        self.critic = REGISTRY[args.critic](args, None)
-
-        # 定义优化器
         self.actor_optimizer = optim.Adam(self.runner.controller.agent.parameters(), lr=args.actor_lr)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=args.critic_lr)
-
 
     def learn(self, buffer, t_env, episode_num):
         pass

06519ec10e1464340092029433f0c2f3f1bc6b81
XiangqiUESTC <402214886@qq.com>
2026-04-20 18:47:38 +0800

    新增了一个对离散空间做Onehot编码的wrapper

--- src/wrapper/__init__.py
+++ src/wrapper/__init__.py
@@ -1,7 +1,8 @@
-from .common import StateToTensor, RewardToTensor, AllToTensor, FlagToTensor
+from .common import AllToTensor, FlagToTensor, RewardToTensor, StateToOneHot, StateToTensor
 
 REGISTRY = {
     "state_to_tensor": StateToTensor,
+    "state_to_one_hot": StateToOneHot,
     "reward_to_tensor": RewardToTensor,
     "flag_to_tensor": FlagToTensor,
     "all_to_tensor": AllToTensor,

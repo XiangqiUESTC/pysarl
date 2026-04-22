0a5823d055ab325c30c8249712245d5cbbadd6b7
XiangqiUESTC <402214886@qq.com>
2026-04-22 14:44:21 +0800

    buffer只保留最新的

--- src/buffers/__init__.py
+++ src/buffers/__init__.py
@@ -1,6 +1,5 @@
 from .basic_buffer import BasicBuffer
-from .new_buffer import BasicBuffer as NewBasicBuffer
 
 REGISTRY = {
-    "basic_buffer": NewBasicBuffer,
+    "basic_buffer": BasicBuffer,
 }

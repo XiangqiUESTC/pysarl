2774983d78da9de40d6f51c64936c99d9b93ad63
XiangqiUESTC <402214886@qq.com>
2026-04-22 14:43:01 +0800

    增加dqn和sarsa算法

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,9 +1,13 @@
 from .dqn import DQN
 from .reinforce import Reinforce
 from .actor_critic import ActorCritic
+from .q_learning import QLearning
+from .sarsa import Sarsa
 
 REGISTER = {
     "dqn": DQN,
     "reinforce": Reinforce,
-    "actor_critic": ActorCritic
+    "actor_critic": ActorCritic,
+    "q_learning": QLearning,
+    "sarsa": Sarsa,
 }
